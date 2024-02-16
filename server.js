fab16bf17cd8b302bb88b8dfd49290a603b45940
Lex <info@alexandranel.com>
2024-02-17 10:32:48 +1100

    have set up the server to PORT 3001 to serve the static files form the public folder

---
+++ server.js
@@ -0,0 +1,13 @@
+// Import express 
+const express = require('express');
+// initialise express into variable 'app'
+const app = express();
+//insitialising the PORT for the application in variable POT at 3001
+const PORT = 3001;
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`Epress Note Taker is listening at http://localhost:${PORT}`)
+);
