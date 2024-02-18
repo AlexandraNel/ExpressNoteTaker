01553b45b6eb8f53715af6e6abb3ff2ee59b17ce
Lex <info@alexandranel.com>
2024-02-18 21:24:08 +1100

    have updated my files and linked correctly. have begun the notes get request

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-// Import express 
+//importing express and initialising it into variable "app"
 const express = require('express');
-// initialise express into variable 'app'
 const app = express();
-//insitialising the PORT for the application in variable POT at 3001
-const PORT = 3001;
+const PORT = 3001; //specifying PORT into a variable as 3001
+const path = require('path'); //path library initialised for use in concatenating path names etc 
+const notesData = require('./db/db.json'); //access to the json db for storing notes
 
-// Serve static files from the public directory
-app.use(express.static('public'));
+app.use(express.json()); //Middleware for parsing json data
+app.use(express.static('public')); //access to static files ie. index.html (now accessed automatically on calling the localhost)
+app.use(express.urlencoded({ extended: true })); //express middleware assiting with parsing form submission with web browsers
 
-app.listen(PORT, () =>
-  console.log(`Express Note Taker is listening at http://localhost:${PORT}`)
-);
+
+
+//href on index.html Get Started button leads to /notes
+//this will present the nottes.html to the user when they press the Get Started button.
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/notes.html'));  
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notesData);
+    console.log(notesData); 
+})
+
+//running ther server on PORT
+app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}`));
